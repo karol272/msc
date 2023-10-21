0ac2c4f9d62f05a7e3aac8d3a13d0f84a16260ed
karol272 <49593087+karol272@users.noreply.github.com>
2023-10-21 11:38:05 -0500

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 19:29:17 2023
+
+@author: dcromeroc
+"""
+
+"""myfile2.py"""
+A= 7 +8
+print("A" ,A)
+print("agregar algunas lineas mas")
+print("agregar algunas lineas mas")
+print("agregar algunas lineas mas")
